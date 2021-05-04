7167f71831dc198c6d36007450de9dafe12d2d73
reyes2981 <noe.reyes92@yahoo.com>
2021-05-04 17:39:10 -0500

    refactor form into JS from HTML

--- src/appointment.js
+++ src/appointment.js
@@ -1,5 +1,6 @@
 class Appointment {
 
+
     constructor(appointment, appointmentAttributes) { // constructor needs to be correct!
         this.id = appointment.id
         this.first_name = appointmentAttributes.first_name
@@ -9,8 +10,102 @@ class Appointment {
         this.hairdresser = appointmentAttributes.hairdresser
         this.datetime = appointmentAttributes.datetime
     }
+    
 
 
+    // renders instances of Appointments
+
+    renderInnerHTML = () => { 
+        return `
+        <div class="form-group">
+            <label for="first name">First Name</label>
+            <input type="text" name="first name" id="FirstName" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="last name">Last Name</label>
+            <input type="text" name="last name" id="LastName" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="email">Email</label>
+            <input type="text" name="email" id="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <label for="item-hairdresser">Stylists</label>
+            <select id="appt-hairdresser" name="appt" class="form-control">
+                <option value="1">Elsa</option>
+                <option value="2">Lucy</option>
+                <option value="3">Margarita</option>
+                <option value="4">Sia</option>
+            </select>
+        </div>
+        <div class="form-group">
+        <label for="item-service">Services</label>
+        <select id="appt-service" name="appt" class="form-control">
+            <option value="1">Haircut</option>
+            <option value="2">Styling</option>
+            <option value="3">Color</option>
+            <option value="4">Waxing</option>
+            <option value="5">Perm</option>
+            <option value="6">Other</option>
+        </select>
+    </div>
+        <input type="submit" name="submit" value="Schedule Appointmnet" class="btn btn-primary btn-block">
+    `
+    }
+
+    
+
+
+    getAppointments() {
+        fetch(endPoint)
+        .then(response => response.json())
+        .then(appointments => {
+            console.log(appointments)
+            appointments.data.forEach(appointment => { // loop 
+                let newAppointment = new Appointment(appointment, appointment.attributes)
+                document.querySelector('#appointment-container').innerHTML += newAppointment.renderAppointmentCard()
+            })
+        })
+        console.log("hello from getAppointments")
+    }
+    
+    /* renderInnerHTML = () => { 
+        return `
+            <div class="form-group">
+                <label for="first name">First Name</label>
+                <input type="text" name="first name" id="FirstName" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="last name">Last Name</label>
+                <input type="text" name="last name" id="LastName" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" name="email" id="email" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="item-hairdresser">Stylists</label>
+                <select id="appt-hairdresser" name="appt" class="form-control">
+                    <option value="1">Elsa</option>
+                    <option value="2">Lucy</option>
+                    <option value="3">Margarita</option>
+                    <option value="4">Sia</option>
+                </select>
+            </div>
+            <div class="form-group">
+            <label for="item-service">Services</label>
+            <select id="appt-service" name="appt" class="form-control">
+                <option value="1">Haircut</option>
+                <option value="2">Styling</option>
+                <option value="3">Color</option>
+                <option value="4">Waxing</option>
+                <option value="5">Perm</option>
+                <option value="6">Other</option>
+            </select>
+        </div>
+            <input type="submit" name="submit" value="Schedule Appointmnet" class="btn btn-primary btn-block">
+        `
+    }*/
 
     renderAppointmentCard() {  // render function 
               return `
@@ -23,18 +118,11 @@ class Appointment {
               <p>${this.datetime}</p>
               <button onClick="removeAppt()" class="delete-appt-button">Delete Appointment</button>
               </div>
-              <br><br>
-
-              `;
-
-              
-
+              <br><br>`;
+      
     }
 
-
 }
-
-
-
+   
 
 // Appointment.all = [];
--- src/index.js
+++ src/index.js
@@ -1,19 +1,56 @@
 // global variables
-
 const toggleStylists = document.getElementById('hairdresser-container')
 const endPoint = "http://localhost:3000/api/v1/appointments" 
+const selectApptBttn = document.getElementById("toggleForm");
+
 
 document.addEventListener("DOMContentLoaded", () => {
     // fetch & load appointments
-    console.log("DOM Content Loaded")
-    getAppointments()
-    getHairdressers()
-    
-    const createAppointmentForm = document.querySelector("#create-appointment-form")
-    createAppointmentForm.addEventListener("submit", (e) => createFormHandler(e))
+    console.log("DOM Content Loaded");
+    //getAppointments();
+
 })
 
-function toggleForm() {
+selectApptBttn.addEventListener("click", () => {
+    // fetch & load appointments
+    create_form();
+    //getAppointments();
+})
+
+function create_form() {
+  // Create a form synamically
+  const form = document.createElement("form");
+  form.setAttribute("method", "post");
+  form.setAttribute("action", "submit.php");
+
+  const FN = document.createElement("input");
+  FN.setAttribute("type", "text");
+  FN.setAttribute("name", "FirstName");
+  FN.setAttribute("id", "FirstName");
+  FN.setAttribute("placeholder", "FirstName");
+
+  const submit = document.createElement("input");
+  submit.setAttribute("type", "submit");
+  submit.setAttribute("value", "Submit");
+  
+                // Append the email_ID input to the form
+  form.append(FN); 
+                
+                // Append the button to the form
+  form.append(submit); 
+  
+  document.getElementsByTagName("body")[0].appendChild(form);
+
+  console.log("hello from create_form");
+}
+
+
+
+
+
+
+
+/*function toggleForm() {
     var x = document.querySelector("form#create-appointment-form");
     if (x.style.display === "none") {
       x.style.display = "block";
@@ -21,9 +58,9 @@ function toggleForm() {
       x.style.display = "none";
     }
     console.log("hello from the toggleForm function");
-}
+}*/
 
-function toggleAppts() {
+/*function toggleAppts() {
     var x = document.querySelector("#appointment-container");
     if (x.style.display === "none") {
       x.style.display = "block";
@@ -31,9 +68,9 @@ function toggleAppts() {
       x.style.display = "none";
     }
     console.log("hello from the toggleAppts function");
-}
+}*/
 
-function getAppointments() {
+/*function getAppointments() {
     fetch(endPoint)
     .then(response => response.json())
     .then(appointments => {
@@ -43,19 +80,7 @@ function getAppointments() {
             document.querySelector('#appointment-container').innerHTML += newAppointment.renderAppointmentCard()
         })
     })
-}
-
-function getHairdressers() {
-    fetch('http://localhost:3000/api/v1/hairdressers')
-    .then(response => response.json())
-    .then(hairdressers => {
-        console.log(hairdressers)
-        hairdressers.data.forEach(hairdresser => { // loop 
-            let newhairdresser = new Hairdresser(hairdresser, hairdresser.attributes)
-            document.querySelector('#hairdresser-container').innerHTML += newhairdresser.renderHairdresserCard()
-        })
-    })
-}
+}*/
 
 function createFormHandler(e) {
     e.preventDefault()
@@ -87,26 +112,60 @@ function postAppointment(first_name, last_name, email, service_id, hairdresser_i
     })
 }
 
-function hideForm() {
+/*function hideForm() {
     var x = document.querySelector("form#create-appointment-form");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-}
+}*/
 
-function showHairdresser() {
+
+/*function showHairdresser() {
     var x = document.querySelector("#hairdresser-container");
     if (x.style.display === "none") {
       x.style.display = "block";
     } else {
       x.style.display = "none";
     }
-}
+}*/
 
 // destroys instance of appointment
-
-function removeAppt() {
-  console.log("greetings from removeAppt");
-}
+var down = document.getElementById("GFG_DOWN");
+            function GFG_Fun() {
+                
+                // Create a form synamically
+                var form = document.createElement("form");
+                form.setAttribute("method", "post");
+                form.setAttribute("action", "submit.php");
+  
+                // Create an input element for emailID
+                var ID = document.createElement("input");
+                ID.setAttribute("type", "text");
+                ID.setAttribute("name", "emailID");
+                ID.setAttribute("placeholder", "E-Mail ID");
+  
+                // Create an input element for password
+                var PWD = document.createElement("input");
+                PWD.setAttribute("type", "password");
+                PWD.setAttribute("name", "password");
+                PWD.setAttribute("placeholder", "Password");
+  
+                // Create a submit button
+                var s = document.createElement("input");
+                s.setAttribute("type", "submit");
+                s.setAttribute("value", "Submit");
+  
+                // Append the email_ID input to the form
+                form.append(ID); 
+                
+                // Append the password to the form
+                form.append(PWD); 
+                
+                // Append the button to the form
+                form.append(s); 
+  
+                document.getElementsByTagName("body")[0]
+               .appendChild(form);
+            }
